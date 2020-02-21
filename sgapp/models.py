1fd823b5af99f996da30e7ba9634828b4e3360db
i-cluster <jhlee9091@gmail.com>
2020-02-21 21:26:43 +0900

    juntest

--- sgapp/models.py
+++ sgapp/models.py
@@ -15,6 +15,7 @@ class Comment(models.Model):
     objects = models.Manager()
     crs = models.ForeignKey('sgapp.Course', on_delete=models.CASCADE, related_name='comment')
     content = models.CharField(max_length=300)
+    score = models.IntegerField(default=0)
     date = models.DateTimeField(default=timezone.now)
     author = models.CharField(max_length=30, null=True, blank=True)
 
