950952eda83896a75626ea8fa0858b03482e3398
Seohyun <shkahng0205@likelion.org>
2020-02-24 02:22:48 +0900

    seohyun mypage_revised&js_added

--- sgapp/static/js/javascript.js
+++ sgapp/static/js/javascript.js
@@ -1,4 +1,4 @@
-// 등록 이미지 등록 미리보기
+//이미지 미리보기
 function readInputFile(input) {
     if(input.files && input.files[0]) {
         var reader = new FileReader();
@@ -13,23 +13,3 @@ $(".inp-img").on('change', function(){
     readInputFile(this);
 });
  
- 
-// 등록 이미지 삭제 ( input file reset )
-function resetInputFile($input, $preview) {
-    var agent = navigator.userAgent.toLowerCase();
-    if((navigator.appName == 'Netscape' && navigator.userAgent.search('Trident') != -1) || (agent.indexOf("msie") != -1)) {
-        // ie 일때
-        $input.replaceWith($input.clone(true));
-        $preview.empty();
-    } else {
-        //other
-        $input.val("");
-        $preview.empty();
-    }       
-}
- 
-$(".btn-delete").click(function(event) {
-    var $input = $(".inp-img");
-    var $preview = $('#preview');
-    resetInputFile($input, $preview);
-});
