9ee99bece1469ca74c6433a415c940c6571bb970
i-cluster <jhlee9091@gmail.com>
2020-02-24 21:46:15 +0900

    deploy

--- sogeun/settings.py
+++ sogeun/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'g7$0+58wg$@ab4d8-yr1-bdwkadn$k#m4s=$3w#ydql996#dmi'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'g7$0+58wg$@ab4d8-yr1-bdwkadn$k#m4s=$3w#ydql996#dmi')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
